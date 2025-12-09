5438fdd8043bd076e71f258cb0305b412a32890a
Kuhali Paul <paulkuhali99@gmail.com>
2025-12-10 02:30:07 +0530

    Navbar and Hero section is done commit

---
+++ src/sections/ContactSummary.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const ContactSummary = () => {
+	return (
+		<>
+			<div>Conact Summary</div>
+		</>
+	);
+};
+
+export default ContactSummary;
---
+++ src/sections/ServiceSummary.jsx
@@ -0,0 +1,9 @@
+export const ServiceSummary = () => {
+	return (
+		<section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
+			<div className="">
+				<p>Architucture</p>
+			</div>
+		</section>
+	);
+};
